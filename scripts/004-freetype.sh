b65e79875a26da961dca153a2ef3945c5e016a3e
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:17:14 +0100

    Upgrade freetype to 2.11.1, turn off unpack verbose, added macOS number threads detection

--- scripts/004-freetype.sh
+++ scripts/004-freetype.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
-# freetype-2.4.3.sh by Naomi Peori (naomi@peori.ca)
+# freetype.sh by Naomi Peori (naomi@peori.ca)
+
+VER=2.11.1
 
 ## Download the source code.
-wget --continue http://download.savannah.gnu.org/releases/freetype/freetype-old/freetype-2.4.3.tar.gz
+wget --continue https://download.savannah.gnu.org/releases/freetype/freetype-${VER}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf freetype-2.4.3 && tar xfvz freetype-2.4.3.tar.gz && cd freetype-2.4.3
+rm -Rf freetype-${VER} && tar xfz freetype-${VER}.tar.gz && cd freetype-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub builds/unix/
@@ -24,7 +26,9 @@ which gmake 1>/dev/null 2>&1 && MAKE=gmake
 CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
 LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
 PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
-GNUMAKE=$MAKE ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
+GNUMAKE=$MAKE ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared --with-brotli=no --with-harfbuzz=no
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
