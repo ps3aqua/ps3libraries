8a9e58ce37037737b9e2168d585fc10d93fc29b5
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:17:46 +0100

    Upgrade libogg to 1.3.5, turn off unpack verbose, added macOS number threads detection

--- scripts/007-libogg.sh
+++ scripts/007-libogg.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
-# libogg-1.2.1.sh by Naomi Peori (naomi@peori.ca)
+# libogg.sh by Naomi Peori (naomi@peori.ca)
+
+VER=1.3.5
 
 ## Download the source code.
-wget --continue http://downloads.xiph.org/releases/ogg/libogg-1.2.1.tar.gz
+wget --continue http://downloads.xiph.org/releases/ogg/libogg-${VER}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf libogg-1.2.1 && tar xfvz libogg-1.2.1.tar.gz && cd libogg-1.2.1
+rm -Rf libogg-${VER} && tar xfz libogg-${VER}.tar.gz && cd libogg-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
@@ -24,4 +26,6 @@ PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
