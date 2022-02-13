31cdb0cdfb35af075a3beb4a1fcc004cb48e24a6
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:21:01 +0100

    Upgrade libcurl to 7.81.0, turn off unpack verbose, added macOS number threads detection

--- scripts/016-libcurl.sh
+++ scripts/016-libcurl.sh
@@ -1,22 +1,27 @@
 #!/bin/sh -e
-# libcurl-7.31.0 by KaKaRoTo
-# modified by mhaqs for 7.31.0 release and cpp compatibility
+# libcurl.sh by KaKaRoTo
+# modified by mhaqs for 7.41.0 release and cpp compatibility
+
+VER=7.81.0
 
 ## Download the source code.
-wget --continue http://curl.haxx.se/download/curl-7.31.0.tar.gz
+wget --continue http://curl.haxx.se/download/curl-${VER}.tar.gz
+
+wget https://curl.haxx.se/ca/cacert.pem
+mv cacert.pem $PSL1GHT/
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf curl-7.31.0 && tar xfvz curl-7.31.0.tar.gz && cd curl-7.31.0
+rm -Rf curl-${VER} && tar xfz curl-${VER}.tar.gz && cd curl-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
 
 ## Patch the source code.
-cat ../../patches/libcurl-7.31.0.patch | patch -p1
+cat ../../patches/libcurl-${VER}.patch | patch -p1
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
@@ -29,7 +34,12 @@ AR="ppu-ar" CC="ppu-gcc" RANLIB="ppu-ranlib" \
   LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib" LIBS="-lnet -lsysutil -lsysmodule -lm " \
   PKG_CONFIG_LIBDIR="$PSL1GHT/ppu/lib/pkgconfig" PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
      ../configure   --prefix="$PS3DEV/portlibs/ppu"  --host="powerpc64-ps3-elf"  \
-          --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib" --without-ssl --with-polarssl="$PS3DEV/portlibs/ppu/include/polarssl" --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt"
+          --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib" \
+          --without-ssl --with-polarssl="$PS3DEV/portlibs/ppu/include/polarssl" \
+          --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt" --disable-threaded-resolver \
+          --disable-ipv6 --with-mbedtls --disable-ntlm-wb
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
