91dfeade2a9c582f0fb594f2f2f3e452f046c647
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:23:59 +0100

    Added a52dec library

---
+++ scripts/023-a52dec.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+VER=0.7.4
+
+## Download the source code.
+wget --continue https://liba52.sourceforge.io/files/a52dec-${VER}.tar.gz
+
+## Download an up-to-date config.guess and config.sub
+if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+
+## Unpack the source code.
+rm -Rf a52dec-${VER} && tar xfz a52dec-${VER}.tar.gz && cd a52dec-${VER}
+
+## Replace config.guess and config.sub
+cp ../config.guess ../config.sub .
+
+autoreconf -fi
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu
+
+## Configure the build.
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
+PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
+
+## Compile and install.
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS -C liba52 && ${MAKE:-make} -C liba52 install && ${MAKE:-make} -C include install
