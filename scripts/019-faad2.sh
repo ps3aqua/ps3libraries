172be214f1b4c779954946fc58f5f1ad44b781b1
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:22:44 +0100

    Upgrade faad2 to 2.10, turn off unpack verbose, added macOS number threads detection

--- scripts/019-faad2.sh
+++ scripts/019-faad2.sh
@@ -1,18 +1,20 @@
 #!/bin/sh -e
-# faad2-2.7.sh by dhewg (dhewg@wiibrew.org)
+# faad2.sh by dhewg (dhewg@wiibrew.org)
 
 ## Download the source code.
-wget --continue http://downloads.sourceforge.net/faac/faad2-2.7.tar.gz
+wget --continue https://github.com/knik0/faad2/archive/refs/tags/2_10_0.tar.gz -O faad2-2.10.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf faad2-2.7 && tar xfvz faad2-2.7.tar.gz && cd faad2-2.7
+rm -Rf faad2-2.10 && tar xfz faad2-2.10.tar.gz && cd faad2-2_10_0
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
+
+NOCONFIGURE=1 ./bootstrap
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
@@ -24,4 +26,6 @@ PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
