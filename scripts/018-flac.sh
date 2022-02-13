56e49ecb7a88da6e25aa066ac555c94c776155e7
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:22:10 +0100

    Update flac, turn off unpack verbose, added macOS number threads detection

--- scripts/018-flac.sh
+++ scripts/018-flac.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
-# flac-1.2.1.sh by dhewg (dhewg@wiibrew.org)
+# flac.sh by dhewg (dhewg@wiibrew.org)
+
+VER=1.2.1
 
 ## Download the source code.
-wget --continue http://downloads.sourceforge.net/project/flac/flac-src/flac-1.2.1-src/flac-1.2.1.tar.gz
+wget --continue http://downloads.sourceforge.net/project/flac/flac-src/flac-${VER}-src/flac-${VER}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf flac-1.2.1 && tar xfvz flac-1.2.1.tar.gz && cd flac-1.2.1
+rm -Rf flac-${VER} && tar xfz flac-${VER}.tar.gz && cd flac-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
@@ -24,4 +26,6 @@ PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared --enable-altivec --disable-xmms-plugin --disable-ogg --disable-oggtest
 
 ## Compile (only parts to prevent failures in unrequired parts) and install.
-${MAKE:-make} -C src/libFLAC -j4 && ${MAKE:-make} -C src/libFLAC install && ${MAKE:-make} -C include install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -C src/libFLAC -j $PROCS && ${MAKE:-make} -C src/libFLAC install && ${MAKE:-make} -C include install
