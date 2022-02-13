67e2c4307072fc6dd4ecdb93655f4daaff1fd5b1
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:16:21 +0100

    Upgrade jpeg to 9e, turn off unpack verbose, added macOS number threads detection

--- scripts/003-jpeg.sh
+++ scripts/003-jpeg.sh
@@ -1,21 +1,20 @@
 #!/bin/sh -e
-# jpeg-8b.sh by Naomi Peori (naomi@peori.ca)
+# jpeg.sh by Naomi Peori (naomi@peori.ca)
+
+VER=9e
 
 ## Download the source code.
-wget --continue http://www.ijg.org/files/jpegsrc.v8b.tar.gz
+wget --continue http://www.ijg.org/files/jpegsrc.v${VER}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf jpeg-8b && tar xfvz jpegsrc.v8b.tar.gz && cd jpeg-8b
+rm -Rf jpeg-${VER} && tar xfz jpegsrc.v${VER}.tar.gz && cd jpeg-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
-
-## Patch the source code.
-cat ../../patches/jpeg-8b-PPU.patch | patch -p1
 
 ## Create the build directory.
 mkdir build-ppu && cd build-ppu
@@ -27,5 +26,6 @@ PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
-
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
