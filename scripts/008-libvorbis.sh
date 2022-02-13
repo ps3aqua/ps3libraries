c7ad84c08929acc7244c3969589ccc9878a3b3d3
Paweł Kołodziejski <aquadran@gmail.com>
2022-02-13 20:18:23 +0100

    Upgrade libvorbis to 1.3.7, turn off unpack verbose, added macOS number threads detection

--- scripts/008-libvorbis.sh
+++ scripts/008-libvorbis.sh
@@ -1,15 +1,17 @@
 #!/bin/sh -e
-# libvorbis-1.3.2.sh by Naomi Peori (naomi@peori.ca)
+# libvorbis.sh by Naomi Peori (naomi@peori.ca)
+
+VER=1.3.7
 
 ## Download the source code.
-wget --continue http://downloads.xiph.org/releases/vorbis/libvorbis-1.3.2.tar.gz
+wget --continue http://downloads.xiph.org/releases/vorbis/libvorbis-${VER}.tar.gz
 
 ## Download an up-to-date config.guess and config.sub
 if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
 if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
 
 ## Unpack the source code.
-rm -Rf libvorbis-1.3.2 && tar xfvz libvorbis-1.3.2.tar.gz && cd libvorbis-1.3.2
+rm -Rf libvorbis-${VER} && tar xfz libvorbis-${VER}.tar.gz && cd libvorbis-${VER}
 
 ## Replace config.guess and config.sub
 cp ../config.guess ../config.sub .
@@ -24,4 +26,6 @@ PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
 ../configure --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
 
 ## Compile and install.
-${MAKE:-make} -j4 && ${MAKE:-make} install
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
