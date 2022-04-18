bc619b22f775dd0a9403f631cbe3a2acc1a73fb5
Paweł Kołodziejski <aquadran@gmail.com>
2022-04-18 10:49:11 +0200

    Updated zlib to 1.2.12 and fixed URL

--- scripts/001-zlib.sh
+++ scripts/001-zlib.sh
@@ -1,16 +1,16 @@
 #!/bin/sh -e
 # zlib.sh by Naomi Peori (naomi@peori.ca)
 
-VER=1.2.11
+VER=1.2.12
 
 ## Download the source code.
-wget --continue http://zlib.net/zlib-1.2.11.tar.gz
+wget --continue https://www.zlib.net/fossils/zlib-${VER}.tar.gz
 
 ## Unpack the source code.
-rm -Rf zlib-1.2.11 && tar xfz zlib-1.2.11.tar.gz && cd zlib-1.2.11
+rm -Rf zlib-${VER} && tar xfz zlib-${VER}.tar.gz && cd zlib-${VER}
 
 ## Patch the source code.
-cat ../../patches/zlib-1.2.11-PPU.patch | patch -p1
+cat ../../patches/zlib-${VER}-PPU.patch | patch -p1
 
 ## Configure the build.
 AR="powerpc64-ps3-elf-ar" CC="powerpc64-ps3-elf-gcc" RANLIB="powerpc64-ps3-elf-ranlib" \
