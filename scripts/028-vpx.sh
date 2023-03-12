cda51ed9c0253ea9d75161ff343333fb4e68ca0b
Paweł Kołodziejski <aquadran@gmail.com>
2023-03-12 12:26:37 +0100

    added libvpx 1.13.0

---
+++ scripts/028-vpx.sh
@@ -0,0 +1,32 @@
+#!/bin/sh -e
+
+VER=1.13.0
+
+## Download the source code.
+wget --continue https://github.com/webmproject/libvpx/archive/v${VER}/libvpx-${VER}.tar.gz
+
+## Download an up-to-date config.guess and config.sub
+if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+
+## Unpack the source code.
+rm -Rf libvpx-${VER} && tar xzf libvpx-${VER}.tar.gz && cd libvpx-${VER}
+
+## Replace config.guess and config.sub
+cp ../config.guess ../config.sub .
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu
+
+## Configure the build.
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
+PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+CROSS=powerpc64-ps3-elf- \
+../configure --prefix="$PS3DEV/portlibs/ppu" --target=generic-gnu --disable-shared --disable-multithread \
+--disable-examples --disable-tools --disable-docs
+
+## Compile and install.
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
