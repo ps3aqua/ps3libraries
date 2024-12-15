81a280c0c40d06f18db5a4278cfa37be9b018464
Paweł Kołodziejski <aquadran@gmail.com>
2024-12-15 10:51:17 +0100

    added libmpcdec 1.2.6

---
+++ scripts/029-libmpcdec.sh
@@ -0,0 +1,31 @@
+#!/bin/sh -e
+
+VER=1.2.6
+
+## Download the source code.
+wget --continue https://files.musepack.net/source/libmpcdec-${VER}.tar.bz2
+
+## Download an up-to-date config.guess and config.sub
+if [ ! -f config.guess ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.guess; fi
+if [ ! -f config.sub ]; then wget --continue http://git.savannah.gnu.org/cgit/config.git/plain/config.sub; fi
+
+## Unpack the source code.
+rm -Rf libmpcdec-${VER} && tar xf libmpcdec-${VER}.tar.bz2 && cd libmpcdec-${VER}
+
+## Replace config.guess and config.sub
+cp ../config.guess ../config.sub .
+
+## Create the build directory.
+mkdir build-ppu && cd build-ppu
+
+## Configure the build.
+CFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib -lrt -llv2" \
+PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+CROSS=powerpc64-ps3-elf- \
+../configure ac_cv_func_memcmp_working=1 --prefix="$PS3DEV/portlibs/ppu" --host="powerpc64-ps3-elf" --disable-shared
+
+## Compile and install.
+PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
+if [ ! -z $ret ]; then PROCS="$(sysctl -n hw.ncpu 2>/dev/null)"; fi
+${MAKE:-make} -j $PROCS && ${MAKE:-make} install
