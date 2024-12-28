1ed88cfe1f9d7e20a6362225e4d3a88e63e35f8c
Paweł Kołodziejski <aquadran@gmail.com>
2024-12-28 13:21:45 +0100

    libcurl: disable not used features

--- scripts/016-libcurl.sh
+++ scripts/016-libcurl.sh
@@ -27,17 +27,43 @@ cat ../../patches/libcurl-${VER}.patch | patch -p1
 mkdir build-ppu && cd build-ppu
 
 ## Configure the build.
-AR="ppu-ar" CC="ppu-gcc" RANLIB="ppu-ranlib" \
-  CFLAGS="-O2 -Wall" \
-  CXXFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
-  CPPFLAGS=" -I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include -I$PSL1GHT/ppu/include/net"  \
-  LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib" LIBS="-lnet -lsysutil -lsysmodule -lm " \
-  PKG_CONFIG_LIBDIR="$PSL1GHT/ppu/lib/pkgconfig" PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
-     ../configure   --prefix="$PS3DEV/portlibs/ppu"  --host="powerpc64-ps3-elf"  \
-          --includedir="$PS3DEV/portlibs/ppu/include"   --libdir="$PS3DEV/portlibs/ppu/lib" \
-          --with-mbedtls="$PS3DEV/portlibs/ppu/include/mbedtls" \
-          --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt" --disable-threaded-resolver \
-          --disable-ipv6 --with-mbedtls --disable-ntlm-wb
+AR="ppu-ar" \
+CC="ppu-gcc" \
+RANLIB="ppu-ranlib" \
+CFLAGS="-O2 -Wall" \
+CXXFLAGS="-I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include" \
+CPPFLAGS=" -I$PSL1GHT/ppu/include -I$PS3DEV/portlibs/ppu/include -I$PSL1GHT/ppu/include/net"  \
+LDFLAGS="-L$PSL1GHT/ppu/lib -L$PS3DEV/portlibs/ppu/lib" \
+LIBS="-lnet -lsysutil -lsysmodule -lm " \
+PKG_CONFIG_LIBDIR="$PSL1GHT/ppu/lib/pkgconfig" \
+PKG_CONFIG_PATH="$PS3DEV/portlibs/ppu/lib/pkgconfig" \
+  ../configure --prefix="$PS3DEV/portlibs/ppu" \
+    --host="powerpc64-ps3-elf" \
+    --includedir="$PS3DEV/portlibs/ppu/include" \
+    --libdir="$PS3DEV/portlibs/ppu/lib" \
+    --with-mbedtls="$PS3DEV/portlibs/ppu/include/mbedtls" \
+    --with-ca-bundle="/usr/ssl/certs/ca-bundle.crt" \
+    --disable-threaded-resolver \
+    --disable-ipv6 \
+    --disable-ntlm \
+    --disable-docs \
+    --disable-ftp \
+    --disable-file \
+    --disable-ldap \
+    --disable-ldaps \
+    --disable-rtsp \
+    --disable-proxy \
+    --disable-dict \
+    --disable-telnet \
+    --disable-tftp \
+    --disable-pop3 \
+    --disable-imap \
+    --disable-smb \
+    --disable-smtp \
+    --disable-gopher \
+    --disable-mqtt \
+    --disable-docs \
+    --disable-manual
 
 ## Compile and install.
 PROCS="$(grep -c '^processor' /proc/cpuinfo 2>/dev/null)" || ret=$?
